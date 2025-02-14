ddd97275b70d3c757ae455b95bce218961ae6f7a
Yonatan Getachew Beshah <114074824+jajos12@users.noreply.github.com>
2025-02-14 13:24:06 -0800

    Time: 101 ms (7.14%) | Memory: 18.2 MB (94.38%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -3,7 +3,6 @@ class Solution:
         max_length, hash_one, left = 0, defaultdict(int), 0
         for right in range(len(nums)):
             hash_one[nums[right]] += 1
-            # print(hash_one, max_length)
             while hash_one[0] > k:
                 hash_one[nums[left]] -= 1
                 left += 1
