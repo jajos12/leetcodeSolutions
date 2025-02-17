3178993cf9e9981401c0c91517da59dc80c942e6
Yonatan Getachew Beshah <114074824+jajos12@users.noreply.github.com>
2025-02-17 04:32:19 -0800

    Time: 79 ms (14.98%) | Memory: 41.5 MB (40.49%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -8,11 +8,8 @@ class Solution:
                 my_l[end+1] += -1 if d else 1
         for i in range(1, len(my_l)):
             my_l[i] += my_l[i-1]
-        # print(my_l)
         answer = []
         for i in range(len(s)):
             char = my_l[i] + ord(s[i])
-            # print(ord(s[i]), my_l[i])
-            # print((char + 122)%97%26 + 97, char%97%26)
             answer.append(alpha[(my_l[i] + ord(s[i]) - 97)%26])
         return "".join(answer)
